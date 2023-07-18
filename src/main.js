6b64750a46aa06343cccdf512f05aeeb9a979d2c
alberto.bejenaru <Alberto.bejenaru@edu.iisleviponti.it>
2023-07-18 17:39:53 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import '../style/modern-normalize.css';
+import '../style/style.css';
+import '../style/components/header.css';
+import '../style/components/hero.css';
+import '../style/components/about.css';
+import '../style/components/featured.css';
+import '../style/components/contact.css';
+import '../style/components/footer.css';
+import '../style/components/mobile-nav.css';
+import '../style/utils.css';
+
+import mobileNav from '../utils/mobile-nav';
+// import antimation from '../utils/animation';
+
+
+mobileNav();
+// animation();
